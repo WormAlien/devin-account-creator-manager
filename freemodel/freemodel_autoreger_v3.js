da916e830ff610e23cbbe985c4703b64b41ef5f9
WormAlien <wormalien@github.com>
2026-07-07 01:12:48 +0300

    Revert "freemodel: fallback на instanttempemail при Cloudflare-капче tmailor"
    This reverts commit 4c2bb1ad7dae04039bc94a63bbb3b0c22c0ae837.

--- freemodel/freemodel_autoreger_v3.js
+++ freemodel/freemodel_autoreger_v3.js
@@ -19,7 +19,6 @@ const fs = require("fs");
 const path = require("path");
 const config = require("./config");
 const { CamoufoxTmailor } = require("./lib/camoufox-tmailor-client");
-const { MailProvider } = require("./lib/mail-provider");
 const fmTgBind = require("./lib/fm-tg-bind");
 const tgPool = require("./lib/tg-pool");
 const dashApi = require("../internal/dashboard-api");
@@ -492,16 +491,15 @@ async function registerOne(index, inviteCode) {
       Object.defineProperty(navigator, "webdriver", { get: () => false });
     });
 
-    // Стартуем универсальный mail-провайдер: tmailor (Camoufox) с автопереключением
-    // на instanttempemail.com при Cloudflare-капче.
+    // Стартуем Camoufox-демон для tmailor.
     try {
-      tmailorClient = new MailProvider({ headless: config.HEADLESS !== false, log });
+      tmailorClient = new CamoufoxTmailor({ headless: config.HEADLESS !== false, log });
       await tmailorClient.start();
       const initialMailbox = await tmailorClient.create();
       email = initialMailbox.email;
       accesstoken = initialMailbox.accesstoken;
     } catch (e) {
-      throw new Error(`mail-provider не отвечает: ${e.message}`);
+      throw new Error(`tmailor (Camoufox) не отвечает: ${e.message}`);
     }
 
     // Домены, на которые FreeModel молча НЕ доставляет OTP (форма принимается,
